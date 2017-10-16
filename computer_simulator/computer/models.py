f9f4c44a3cc859d56888963ef0e3af3db1d9b4bc
Juanjo Conti <jjconti@gmail.com>
2017-10-16 16:09:53 -0300

    Add models and migrations

--- computer_simulator/computer/models.py
+++ computer_simulator/computer/models.py
@@ -2,3 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+
+class Computer(models.Model):
+
+    stack_pointer = models.IntegerField(default=0)
+    program_counter = models.IntegerField(default=0)
+    computation_ended = models.BooleanField(default=False)
+
+
+class Register(models.Model):
+
+    address = models.IntegerField()
+    value1 = models.CharField(max_length=50)
+    value2 = models.IntegerField()
+    computer = models.ForeignKey(Computer)
